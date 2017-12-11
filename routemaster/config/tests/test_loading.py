458aec812235bb192f9ba54ab02bc2bb37c3040b
Dan Palmer <dan@danpalmer.me>
2017-12-11 11:19:23 +0000

    Update error messages

--- routemaster/config/tests/test_loading.py
+++ routemaster/config/tests/test_loading.py
@@ -100,22 +100,22 @@ def test_realistic_config():
 
 
 def test_raises_for_action_and_gate_state():
-    with assert_config_error("State at path state_machines.example.0 cannot be both a gate and an action."):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('action_and_gate_invalid'))
 
 
 def test_raises_for_neither_action_nor_gate_state():
-    with assert_config_error("State at path state_machines.example.0 must be either a gate or an action."):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('not_action_or_gate_invalid'))
 
 
 def test_raises_for_no_state_machines():
-    with assert_config_error("No top-level state_machines key defined."):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('no_state_machines_invalid'))
 
 
 def test_raises_for_time_and_context_trigger():
-    with assert_config_error("Trigger at path state_machines.example.0.triggers.0 cannot be both a time and a context trigger."):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('time_and_context_invalid'))
 
 
@@ -125,7 +125,7 @@ def test_raises_for_neither_time_nor_context_trigger():
 
 
 def test_raises_for_invalid_time_format_in_trigger():
-    with assert_config_error("Time trigger '1800' at path state_machines.example.0.triggers.0 does not meet expected format: %Hh%Mm."):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('trigger_time_format_invalid'))
 
 
@@ -135,5 +135,5 @@ def test_raises_for_invalid_path_format_in_trigger():
 
 
 def test_raises_for_neither_constant_no_context_next_states():
-    with assert_config_error("Next state config at path state_machines.example.0.next must be of type 'constant' or 'context'"):
+    with assert_config_error("Could not validate config file against schema."):
         load_config(yaml_data('next_states_not_constant_or_context_invalid'))
