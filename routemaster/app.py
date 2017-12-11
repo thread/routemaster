b4755ef1f8d7cd0797001fb78099a5310cc83967
Dan Palmer <dan@danpalmer.me>
2017-12-11 16:09:34 +0000

    Fix types

--- routemaster/app.py
+++ routemaster/app.py
@@ -1,6 +1,5 @@
 """Core App singleton that holds state for the application."""
-
-from typing import IO
+from typing import IO, Any, Dict
 
 import yaml
 
@@ -11,9 +10,9 @@ class App(object):
     """Core application state."""
 
     config: Config
-    raw_config: str
+    raw_config: Dict[str, Any]
 
     def load_config(self, config_file: IO[str]) -> None:
         """Load configuration from a file."""
-        self.raw_config = yaml.load(config_file.read())
+        self.raw_config = yaml.load(config_file)
         self.config = load_config(self.raw_config)
