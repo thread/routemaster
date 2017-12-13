43f611a5f874493ecbd805ea9353d856ef84b32e
Dan Palmer <dan@danpalmer.me>
2017-12-13 16:00:53 +0000

    Start sketching the labels endpoints

--- routemaster/server/endpoints.py
+++ routemaster/server/endpoints.py
@@ -6,7 +6,7 @@ from sanic.response import json as json_response
 server = Sanic('routemaster')
 
 
-@server.route("/")
+@server.route('/', methods=['GET'])
 async def status(request):
     """Status check endpoint."""
     async with server.config.app.db.begin() as conn:
@@ -16,3 +16,87 @@ async def status(request):
             'labels': num_labels,
             'state_machines': num_state_machines,
         })
+
+
+@server.route(
+    '/state-machines/<state_machine_name:str>/labels',
+    methods=['GET'],
+)
+async def get_labels(request, state_machine_name):
+    """List the labels in a state machine."""
+    pass
+
+
+@server.route(
+    '/state-machines/<state_machine_name:str>/labels/<label:str>',
+    methods=['GET'],
+)
+async def get_label(request, state_machine_name, label):
+    """
+    Get a label within a given state machine.
+
+    Returns:
+    - 200 Ok: if the label is exists.
+    - 404 Not Found: if the state machine or label does not exist.
+
+    Successful return codes return the full context for the label.
+    """
+    pass
+
+
+@server.route(
+    '/state-machines/<state_machine_name:str>/labels/<label:str>',
+    methods=['POST'],
+)
+async def create_label(request, state_machine_name, label):
+    """
+    Create a label with a given context, and start it in the state machine.
+
+    Returns:
+    - 201 Created: if the label is successfully created and started.
+    - 409 Conflict: if the label already exists in the state machine.
+    - 404 Not Found: if the state machine does not exist.
+    - 400 Bad Request: if the request body is not a valid context.
+
+    Successful return codes return the full created context for the label.
+    """
+    pass
+
+
+@server.route(
+    '/state-machines/<state_machine_name:str>/labels/<label:str>/update',
+    methods=['POST'],
+)
+async def update_label(request, state_machine_name, label):
+    """
+    Update a label in a state machine.
+
+    Triggering progression if necessary according to the state machine
+    configuration. Updates are _merged_ with existing context.
+
+    Returns:
+    - 200 Ok: if the label is successfully updated.
+    - 400 Bad Request: if the request body is not a valid context.
+    - 404 Not Found: if the state machine or label does not exist.
+
+    Successful return codes return the full new context for a label.
+    """
+    pass
+
+
+@server.route(
+    '/state-machines/<state_machine_name:str>/labels/<label:str>',
+    methods=['DELETE'],
+)
+async def delete_label(request, state_machine_name, label):
+    """
+    Delete a label in a state machine.
+
+    Marks a label as deleted, but does not remove it from the database.
+    Deleted labels cannot be updated and will not move state.
+
+    Returns:
+    - 204 No content: if the label is successfully deleted.
+    - 404 Not Found: if the state machine or label does not exist.
+    """
+    pass
