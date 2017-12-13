b307301dc04e70b9472f7df3f96b2ff98229f416
Dan Palmer <dan@danpalmer.me>
2017-12-13 17:06:58 +0000

    Add a first test to the label creation endpoint

--- routemaster/server/tests/test_endpoints.py
+++ routemaster/server/tests/test_endpoints.py
@@ -1,3 +1,39 @@
+import json
+
+import pytest
+
+from routemaster.config import (
+    Gate,
+    NoNextStates,
+    StateMachine,
+    ContextTrigger,
+    ConstantNextState,
+)
+from routemaster.exit_conditions import ExitConditionProgram
+
+TEST_STATE_MACHINES = {
+    'test_machine': StateMachine(
+        name='test_machine',
+        states=[
+            Gate(
+                name='start',
+                triggers=[
+                    ContextTrigger(context_path='should_progress'),
+                ],
+                next_states=ConstantNextState(state='end'),
+                exit_condition=ExitConditionProgram('should_progress = true'),
+            ),
+            Gate(
+                name='end',
+                triggers=[],
+                next_states=NoNextStates(),
+                exit_condition=ExitConditionProgram('false'),
+            ),
+        ],
+    ),
+}
+
+
 async def test_root(app_client):
     client = await app_client()
     response = await client.get('/')
@@ -6,3 +42,12 @@ async def test_root(app_client):
         'state_machines': 0,
         'labels': 0,
     }
+
+
+async def test_create_label(app_client, app_config):
+    client = await app_client(app_config(state_machines=TEST_STATE_MACHINES))
+    response = await client.post(
+        '/state-machines/test_machine/labels/foo',
+        data=json.dumps({'bar': 'baz'}),
+    )
+    assert response.status == 201
