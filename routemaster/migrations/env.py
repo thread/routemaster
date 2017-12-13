b9008b63b8af0dd863c9b3fec6c6de53bc7d4e4b
Alistair Lynn <arplynn@gmail.com>
2017-12-13 18:36:47 +0000

    Add skeleton migrations directory

---
+++ routemaster/migrations/env.py
@@ -0,0 +1,81 @@
+import yaml
+
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from logging.config import fileConfig
+
+from routemaster.config import load_config
+from routemaster.db import initialise_db
+from routemaster.db.model import Base
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def get_database_config():
+    config_file = config.get_main_option('routemaster.config')
+    with open(config_file, 'r') as f:
+        config_yaml = yaml.load(f)
+    routemaster_config = load_config(config_yaml)
+    return routemaster_config.database
+
+
+def run_migrations_offline():
+    """
+    Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    context.configure(
+        url=get_database_config().connstr,
+        target_metadata=target_metadata,
+        literal_binds=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """
+    Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    database_config = get_database_config()
+    engine = initialise_db(database_config, asyncio=False)
+
+    with engine.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
