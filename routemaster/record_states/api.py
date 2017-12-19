ca18f00ef50ebd92e693f11118a361c475767670
Alistair Lynn <arplynn@gmail.com>
2017-12-19 19:31:08 +0000

    Extract helpers for deprecating and undeprecating states

--- routemaster/record_states/api.py
+++ routemaster/record_states/api.py
@@ -114,52 +114,60 @@ def _resync_states_on_state_machine(conn, machine):
     created_states = new_states - old_states
 
     if deleted_states:
-        conn.execute(
+        _deprecate_states(conn, machine, deleted_states)
+
+    if created_states:
+        _create_or_undeprecate_states(conn, created_states, machine)
+
+    any_changes = deleted_states or created_states
+    return any_changes
+
+
+def _create_or_undeprecate_states(conn, created_states, machine):
+    # If any of these are old states which have been reanimated, we
+    # just set the deprecated flag back to False and flag them
+    # updated.
+    undeprecated_names = [
+        x.name
+        for x in conn.execute(
             states.update().where(
                 and_(
                     states.c.state_machine == machine.name,
-                    states.c.name.in_(list(deleted_states)),
+                    states.c.name.in_(list(created_states)),
+                    states.c.deprecated,
                 ),
             ).values(
-                deprecated=True,
+                deprecated=False,
                 updated=func.now(),
-            )
+            ).returning(
+                states.c.name,
+            ),
         )
-    if created_states:
-        # If any of these are old states which have been reanimated, we
-        # just set the deprecated flag back to False and flag them
-        # updated.
-        undeprecated_names = [
-            x.name
-            for x in conn.execute(
-                states.update().where(
-                    and_(
-                        states.c.state_machine == machine.name,
-                        states.c.name.in_(list(created_states)),
-                        states.c.deprecated,
-                    ),
-                ).values(
-                    deprecated=False,
-                    updated=func.now(),
-                ).returning(
-                    states.c.name,
-                ),
-            )
-        ]
+    ]
+    newly_inserted_rows = [
+        {
+            'name': state.name,
+        }
+        for state in machine.states
+        if state.name in created_states and
+           state.name not in undeprecated_names
+    ]
+    if newly_inserted_rows:
+        conn.execute(states.insert().values(
+            state_machine=machine.name,
+            updated=func.now(),
+        ), newly_inserted_rows)
 
-        newly_inserted_rows = [
-            {
-                'name': state.name,
-            }
-            for state in machine.states
-            if state.name in created_states and
-               state.name not in undeprecated_names
-        ]
 
-        if newly_inserted_rows:
-            conn.execute(states.insert().values(
-                state_machine=machine.name,
-                updated=func.now(),
-            ), newly_inserted_rows)
-    any_changes = deleted_states or created_states
-    return any_changes
+def _deprecate_states(conn, machine, deleted_states):
+    conn.execute(
+        states.update().where(
+            and_(
+                states.c.state_machine == machine.name,
+                states.c.name.in_(list(deleted_states)),
+            ),
+        ).values(
+            deprecated=True,
+            updated=func.now(),
+        )
+    )
