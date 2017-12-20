88bcea2f2ee9d4ff7e2731f8aa18fb76ecfa61ae
Alistair Lynn <arplynn@gmail.com>
2017-12-20 20:31:58 +0000

    Add firing tests as a statement of correctness

---
+++ routemaster/config/tests/test_context_trigger_firing.py
@@ -0,0 +1,23 @@
+import pytest
+
+from routemaster.config import ContextTrigger
+
+
+TEST_CASES = [
+    ('foo', {}, False),
+    ('foo', {'foo': True}, True),
+    ('foo', {'foo': False}, True),
+    ('foo', {'bar': True}, False),
+    ('foo', {'foo': True, 'bar': True}, True),
+    ('foo.bar', {'foo': {'bar': True}}, True),
+    ('foo.bar', {'foo': {'bazz': True}}, False),
+    ('foo.bar', {'foo': {}}, False),
+    ('foo.bar', {'foo': {'bar': {'bazz': True}}}, True),
+    ('foo.bar', {'foo': {'bar': {}}}, True),
+]
+
+
+@pytest.mark.parametrize('path, update, should_trigger', TEST_CASES)
+def test_context_trigger(path, update, should_trigger):
+    trigger = ContextTrigger(context_path=path)
+    assert trigger.should_trigger_for_update(update) == should_trigger
