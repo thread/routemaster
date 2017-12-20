cdc73a6fe489c5dd17513c454144f1a664bc5ae1
Alistair Lynn <arplynn@gmail.com>
2017-12-20 20:50:32 +0000

    Tests for the gunicorn application

---
+++ routemaster/tests/test_gunicorn_application.py
@@ -0,0 +1,27 @@
+import pytest
+
+import werkzeug.test
+import werkzeug.testapp
+import werkzeug.wrappers
+
+from routemaster.gunicorn_application import GunicornWSGIApplication
+
+
+@pytest.mark.parametrize('debug', (False, True))
+def test_gunicorn_application_can_be_constructed(debug):
+    application = GunicornWSGIApplication(
+        werkzeug.testapp.test_app,
+        bind='[::1]:0',
+        debug=debug,
+    )
+
+    application.load_config()
+    loaded_wsgi_callable = application.load()
+
+    client = werkzeug.test.Client(
+        loaded_wsgi_callable,
+        werkzeug.wrappers.BaseResponse,
+    )
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data.startswith(b'<!DOCTYPE')
