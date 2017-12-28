5526a3cd2b903b06f0ed2fed5b65f87db20abc4c
Dan Palmer <dan@danpalmer.me>
2017-12-28 13:15:02 +0000

    Test a few more error codes

--- routemaster/server/tests/test_endpoints.py
+++ routemaster/server/tests/test_endpoints.py
@@ -117,6 +117,15 @@ def test_update_label_404_for_not_found_label(client):
     assert response.status_code == 404
 
 
+def test_update_label_404_for_not_found_state_machine(client):
+    response = client.patch(
+        '/state-machines/nonexistent_machine/labels/foo',
+        data=json.dumps({'metadata': {'foo': 'bar'}}),
+        content_type='application/json',
+    )
+    assert response.status_code == 404
+
+
 def test_update_label_400_for_invalid_body(client, create_label):
     create_label('foo', 'test_machine', {})
     response = client.patch(
@@ -124,6 +133,19 @@ def test_update_label_400_for_invalid_body(client, create_label):
         data='not valid json',
         content_type='application/json',
     )
+    assert response.status_code == 400
+
+
+def test_update_label_400_for_no_metadata(client, app_config, create_label):
+    create_label('foo', 'test_machine', {})
+
+    label_metadata = {'bar': 'baz'}
+    response = client.patch(
+        '/state-machines/test_machine/labels/foo',
+        data=json.dumps({'not_metadata': label_metadata}),
+        content_type='application/json',
+    )
+
     assert response.status_code == 400
 
 
