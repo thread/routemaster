c76636d3149b6bb3545cc53ac1719b5675a83443
Dan Palmer <dan@danpalmer.me>
2018-01-03 16:31:03 +0000

    Fail validation if any state machines have been deleted for now

--- routemaster/tests/test_validation.py
+++ routemaster/tests/test_validation.py
@@ -2,13 +2,19 @@ import pytest
 
 from routemaster.config import (
     Gate,
+    Config,
     NoNextStates,
     StateMachine,
     ConstantNextState,
     ContextNextStates,
     ContextNextStatesOption,
 )
-from routemaster.validation import ValidationError, _validate_state_machine
+from routemaster.validation import (
+    ValidationError,
+    _validate_state_machine,
+    _validate_no_deleted_state_machines,
+)
+from routemaster.record_states import record_state_machines
 from routemaster.exit_conditions import ExitConditionProgram
 
 
@@ -149,3 +155,39 @@ def test_label_in_deleted_state_on_per_state_machine_basis(
     )
     with pytest.raises(ValidationError):
         _validate_state_machine(app_config, state_machine)
+
+
+def test_deleted_state_machine_invalid(app_config):
+    record_state_machines(app_config, [
+        StateMachine(
+            name='machine_1',
+            states=[
+                Gate(
+                    name='start',
+                    triggers=[],
+                    next_states=NoNextStates(),
+                    exit_condition=ExitConditionProgram('false'),
+                ),
+            ]
+        )
+    ])
+
+    state_machine = Config(
+        state_machines={
+            'machine_2': StateMachine(
+                name='machine_2',
+                states=[
+                    Gate(
+                        name='start',
+                        triggers=[],
+                        next_states=NoNextStates(),
+                        exit_condition=ExitConditionProgram('false'),
+                    ),
+                ]
+            )
+        },
+        database=None,
+        webhooks=[],
+    )
+    with pytest.raises(ValidationError):
+        _validate_no_deleted_state_machines(app_config, state_machine)
