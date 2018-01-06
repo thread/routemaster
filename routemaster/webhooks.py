707b8ca5e98ab77e69ff692d08bca65a0d7f391a
Dan Palmer <dan@danpalmer.me>
2018-01-06 18:17:07 +0000

    This should be a default

--- routemaster/webhooks.py
+++ routemaster/webhooks.py
@@ -26,7 +26,7 @@ class RequestsWebhookRunner(object):
     Optionally takes a list of webhook configs to modify how requests are made.
     """
 
-    def __init__(self, webhook_configs: List[Webhook]) -> None:
+    def __init__(self, webhook_configs: List[Webhook]=[]) -> None:
         self.session = requests.Session()
         self.webhook_configs = webhook_configs
 
