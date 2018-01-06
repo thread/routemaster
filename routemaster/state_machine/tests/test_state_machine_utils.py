532f8a1c4ca343eaa0fc4759b2e30f4237165052
Dan Palmer <dan@danpalmer.me>
2018-01-06 17:48:09 +0000

    Test another case here

--- routemaster/state_machine/tests/test_state_machine_utils.py
+++ routemaster/state_machine/tests/test_state_machine_utils.py
@@ -1,5 +1,6 @@
 import pytest
 
+from routemaster.webhooks import WebhookResult
 from routemaster.state_machine import utils
 from routemaster.state_machine.types import LabelRef
 from routemaster.state_machine.exceptions import UnknownStateMachine
@@ -35,3 +36,19 @@ def test_needs_gate_evaluation_for_metadata_change(app_config, create_label):
             {'should_progress': 'bar'},
             conn,
         ) is True
+
+
+def test_needs_gate_evaluation_for_metadata_change_with_action(app_config, create_label, mock_webhook):
+    label = LabelRef('foo', 'test_machine')
+    (state_machine,) = app_config.config.state_machines.values()
+
+    with mock_webhook(WebhookResult.FAIL):
+        create_label(label.name, state_machine.name, {'should_progress': True})
+
+    with app_config.db.begin() as conn:
+        assert utils.needs_gate_evaluation_for_metadata_change(
+            state_machine,
+            label,
+            {},
+            conn,
+        ) is False
