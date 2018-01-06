6aa24cc4ded9b3c5108d2c7b89c2f51814ef1589
Dan Palmer <dan@danpalmer.me>
2018-01-06 17:35:37 +0000

    Add tests for some utils

---
+++ routemaster/state_machine/tests/test_state_machine_utils.py
@@ -0,0 +1,37 @@
+import pytest
+
+from routemaster.state_machine import utils
+from routemaster.state_machine.types import LabelRef
+from routemaster.state_machine.exceptions import UnknownStateMachine
+
+
+def test_get_state_machine(app_config):
+    label = LabelRef(name='foo', state_machine='test_machine')
+    (state_machine,) = app_config.config.state_machines.values()
+    assert utils.get_state_machine(app_config, label) == state_machine
+
+
+def test_get_state_machine_not_found(app_config):
+    label = LabelRef(name='foo', state_machine='nonexistent_machine')
+    with pytest.raises(UnknownStateMachine):
+        utils.get_state_machine(app_config, label)
+
+
+def test_needs_gate_evaluation_for_metadata_change(app_config, create_label):
+    label = LabelRef(name='foo', state_machine='test_machine')
+    (state_machine,) = app_config.config.state_machines.values()
+    create_label(label.name, state_machine.name, {})
+
+    with app_config.db.begin() as conn:
+        assert utils.needs_gate_evaluation_for_metadata_change(
+            state_machine,
+            label,
+            {'foo': 'bar'},
+            conn,
+        ) is False
+        assert utils.needs_gate_evaluation_for_metadata_change(
+            state_machine,
+            label,
+            {'should_progress': 'bar'},
+            conn,
+        ) is True
