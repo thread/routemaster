ffd5c481fec5b0454c04c5314d57da8bc0e2e95d
Dan Palmer <dan@danpalmer.me>
2018-01-08 14:42:39 +0000

    Give some clarity to how this test works

--- routemaster/state_machine/tests/test_state_machine_utils.py
+++ routemaster/state_machine/tests/test_state_machine_utils.py
@@ -43,11 +43,17 @@ def test_needs_gate_evaluation_for_metadata_change(app_config, create_label):
         ) is True
 
 
-def test_needs_gate_evaluation_for_metadata_change_with_action(app_config, create_label, mock_webhook):
+def test_does_not_need_gate_evaluation_for_metadata_change_with_action(app_config, create_label, mock_webhook):
     (state_machine,) = app_config.config.state_machines.values()
 
+    # Force the action to fail here so that the label is left in the action
+    # state.
     with mock_webhook(WebhookResult.FAIL):
         label = create_label('foo', 'test_machine', {'should_progress': True})
+
+    with app_config.db.begin() as conn:
+        current_state = utils.get_current_state(label, state_machine, conn)
+        assert current_state.name == 'perform_action'
 
     with app_config.db.begin() as conn:
         assert utils.needs_gate_evaluation_for_metadata_change(
