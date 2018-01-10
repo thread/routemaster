b3ac5b1c70f82a7326135a557f0f962aa68f575a
Dan Palmer <dan@danpalmer.me>
2018-01-10 14:43:56 +0000

    Add extra wrapping to allow for more granular processing and error handling.

--- routemaster/cron.py
+++ routemaster/cron.py
@@ -2,12 +2,15 @@
 
 import time
 import threading
+import contextlib
+from typing import Callable, Iterator
 
 import schedule
 
 from routemaster.app import App
 from routemaster.config import (
     Gate,
+    State,
     Action,
     TimeTrigger,
     StateMachine,
@@ -15,21 +18,27 @@ from routemaster.config import (
 )
 
 
-def _trigger_action(action: Action) -> None:
-    print(f"Processing action {action.name}")
+def _trigger_action(app, state, process_part) -> None:
+    print(f"Processing action {state.name}")
 
 
-def _trigger_gate(gate: Gate) -> None:
-    print(f"Processing gate {gate.name}")
+def _trigger_gate(app, state, process_part) -> None:
+    print(f"Processing gate {state.name}")
 
 
 def _configure_schedule_for_state_machine(
+    app: App,
     scheduler: schedule.Scheduler,
     state_machine: StateMachine,
+    is_terminating: Callable[[], bool],
 ) -> None:
+    def _process(fn: CronProcessor, state: State) -> None:
+        return _process_cron_job(is_terminating, fn, app, state)
+
     for state in state_machine.states:
         if isinstance(state, Action):
             scheduler.every().minute.do(
+                _process,
                 _trigger_action,
                 state,
             )
@@ -40,41 +49,94 @@ def _configure_schedule_for_state_machine(
                     scheduler.every().day.at(
                         f"{trigger.time.hour:02d}:{trigger.time.minute:02d}",
                     ).do(
+                        _process,
                         _trigger_gate,
                         state,
                     )
                 elif isinstance(trigger, IntervalTrigger):
                     scheduler.every(
-                        trigger.interval.total_seconds()
+                        trigger.interval.total_seconds(),
                     ).seconds.do(
+                        _process,
                         _trigger_gate,
                         state,
                     )
 
 
-def configure_schedule(scheduler: schedule.Scheduler, app: App) -> None:
+def configure_schedule(
+    app: App,
+    scheduler: schedule.Scheduler,
+    is_terminating: Callable[[], bool],
+) -> None:
     """Set up all scheduled tasks that need running."""
     for state_machine in app.config.state_machines.values():
-        _configure_schedule_for_state_machine(scheduler, state_machine)
+        _configure_schedule_for_state_machine(
+            app,
+            scheduler,
+            state_machine,
+            is_terminating,
+        )
 
 
 class CronThread(threading.Thread):  # pragma: no cover
     """Background thread for running periodic jobs."""
 
     def __init__(self, app: App) -> None:
-        self.terminating = False
+        self._terminating = False
         self.app = app
         self.scheduler = schedule.Scheduler()
         super().__init__(name="cron", daemon=True)
 
     def run(self) -> None:
         """Run main scheduling loop."""
-        configure_schedule(self.scheduler, self.app)
-        while not self.terminating:
+        configure_schedule(self.app, self.scheduler, self.is_terminating)
+        while not self.is_terminating():
             self.scheduler.run_pending()
             time.sleep(1)
 
     def stop(self) -> None:
         """Set the stopping flag and wait for thread end."""
-        self.terminating = True
+        self._terminating = True
         self.join()
+
+    def is_terminating(self) -> bool:
+        """Dynamically access whether we are terminating."""
+        return self._terminating
+
+
+class StopCronProcessing(Exception):
+    """Signal to exit the processing of a series of cron items."""
+    pass
+
+
+ProcessItemContextManager = Callable[
+    [],
+    contextlib.AbstractContextManager,
+]
+CronProcessor = Callable[[App, State, ProcessItemContextManager], None]
+
+
+def _process_cron_job(
+    is_terminating: Callable[[], bool],
+    fn: CronProcessor,
+    app: App,
+    state: State,
+) -> None:
+
+    @contextlib.contextmanager
+    def _inner() -> Iterator[None]:
+        try:
+            yield
+        except Exception as e:
+            # Something went wrong in the processing of a single item in the
+            # cron queue, log it and allow the processor to continue.
+            print(e)
+            return
+
+        if is_terminating():
+            raise StopCronProcessing()
+
+    try:
+        fn(app, state, _inner)
+    except StopCronProcessing:
+        return
--- routemaster/tests/test_cron.py
+++ routemaster/tests/test_cron.py
@@ -34,7 +34,7 @@ def test_action_once_per_minute(custom_app_config):
 
     scheduler = schedule.Scheduler()
     with mock.patch('routemaster.cron._trigger_action') as mock_trigger_action:
-        configure_schedule(scheduler, app)
+        configure_schedule(app, scheduler, lambda: False)
 
     assert len(scheduler.jobs) == 1, "Should have scheduled a single job"
     job, = scheduler.jobs
@@ -46,7 +46,7 @@ def test_action_once_per_minute(custom_app_config):
     with freezegun.freeze_time(job.next_run):
         job.run()
 
-    mock_trigger_action.assert_called_with(action)
+    mock_trigger_action.assert_called_with(app, action, mock.ANY)
 
     assert job.next_run == datetime.datetime(2018, 1, 1, 12, 2)
 
@@ -63,7 +63,7 @@ def test_gate_at_fixed_time(custom_app_config):
 
     scheduler = schedule.Scheduler()
     with mock.patch('routemaster.cron._trigger_gate') as mock_trigger_gate:
-        configure_schedule(scheduler, app)
+        configure_schedule(app, scheduler, lambda: False)
 
     assert len(scheduler.jobs) == 1, "Should have scheduled a single job"
     job, = scheduler.jobs
@@ -75,7 +75,7 @@ def test_gate_at_fixed_time(custom_app_config):
     with freezegun.freeze_time(job.next_run):
         job.run()
 
-    mock_trigger_gate.assert_called_with(gate)
+    mock_trigger_gate.assert_called_with(app, gate, mock.ANY)
 
     assert job.next_run == datetime.datetime(2018, 1, 2, 18, 30)
 
@@ -92,7 +92,7 @@ def test_gate_at_interval(custom_app_config):
 
     scheduler = schedule.Scheduler()
     with mock.patch('routemaster.cron._trigger_gate') as mock_trigger_gate:
-        configure_schedule(scheduler, app)
+        configure_schedule(app, scheduler, lambda: False)
 
     assert len(scheduler.jobs) == 1, "Should have scheduled a single job"
     job, = scheduler.jobs
@@ -104,6 +104,76 @@ def test_gate_at_interval(custom_app_config):
     with freezegun.freeze_time(job.next_run):
         job.run()
 
-    mock_trigger_gate.assert_called_with(gate)
+    mock_trigger_gate.assert_called_with(app, gate, mock.ANY)
 
     assert job.next_run == datetime.datetime(2018, 1, 1, 12, 40)
+
+
+@freezegun.freeze_time('2018-01-01 12:00')
+def test_cron_job_will_exit_gracefully_half_way_through(custom_app_config):
+    gate = Gate(
+        'gate',
+        next_states=NoNextStates(),
+        exit_condition=ExitConditionProgram('false'),
+        triggers=[TimeTrigger(datetime.time(12, 0))],
+    )
+    app = create_app(custom_app_config, [gate])
+
+    items_to_process = ['one', 'two', 'should_not_process']
+
+    def is_terminating():
+        return len(items_to_process) == 1
+
+    def processor(_app, _state, process_wrapper):
+        for item in items_to_process:
+            with process_wrapper():
+                items_to_process.pop(0)
+
+    scheduler = schedule.Scheduler()
+    with mock.patch(
+        'routemaster.cron._trigger_gate',
+        side_effect=processor,
+    ) as mock_trigger_gate:
+        configure_schedule(app, scheduler, is_terminating)
+
+    job, = scheduler.jobs
+    job.run()
+
+    mock_trigger_gate.assert_called_with(app, gate, mock.ANY)
+
+    assert items_to_process == ['should_not_process']
+
+
+@freezegun.freeze_time('2018-01-01 12:00')
+def test_cron_job_process_all_items_even_if_one_errors(custom_app_config):
+    gate = Gate(
+        'gate',
+        next_states=NoNextStates(),
+        exit_condition=ExitConditionProgram('false'),
+        triggers=[TimeTrigger(datetime.time(12, 0))],
+    )
+    app = create_app(custom_app_config, [gate])
+
+    items_to_capitalise = ['one', 2, 'three']
+
+    def processor(_app, _state, process_wrapper):
+        while True:
+            with process_wrapper():
+                item = items_to_capitalise.pop(0)
+                item.upper()
+                if not items_to_capitalise:
+                    return
+
+    scheduler = schedule.Scheduler()
+    with mock.patch(
+        'routemaster.cron._trigger_gate',
+        side_effect=processor,
+    ) as mock_trigger_gate:
+        configure_schedule(app, scheduler, lambda: False)
+
+    job, = scheduler.jobs
+    job.run()
+
+    mock_trigger_gate.assert_called_with(app, gate, mock.ANY)
+
+    assert items_to_capitalise == []
