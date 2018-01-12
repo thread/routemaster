7a6ac4b092950407cd759a6210b948a7c931e2d7
Dan Palmer <dan@danpalmer.me>
2018-01-12 15:53:00 +0000

    Test some more properties of process_transitions

--- routemaster/state_machine/tests/test_transitions.py
+++ routemaster/state_machine/tests/test_transitions.py
@@ -1,11 +1,26 @@
 import mock
 
+from routemaster.state_machine.exceptions import DeletedLabel
 from routemaster.state_machine.transitions import process_transitions
 
 
 def test_cannot_infinite_loop(app_config, create_label, set_metadata):
     label = create_label('foo', 'test_infinite_machine', {})
     set_metadata(label, {'should_progress': True})
+
     with mock.patch('routemaster.state_machine.transitions.logger') as logger:
         process_transitions(app_config, label)
         logger.warn.assert_called_once()
+
+
+def test_stops_on_delete(app_config, create_label, set_metadata):
+    label = create_label('foo', 'test_infinite_machine', {})
+    set_metadata(label, {'should_progress': True})
+
+    with mock.patch(
+        'routemaster.state_machine.transitions.process_gate',
+        side_effect=[mock.DEFAULT, mock.DEFAULT, DeletedLabel],
+    ) as mock_process_gate:
+        process_transitions(app_config, label)
+
+    assert mock_process_gate.call_count == 3
