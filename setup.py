335d316e3dec3278ed4202cc443fead4a2d3b728
Dan Palmer <dan@danpalmer.me>
2018-02-16 09:48:15 +0000

    Move version.py to be more correct.
    If it's in the package there can be issues with importing if deps are
    not installed

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 """Package setup."""
 
+import version
 from setuptools import setup, find_packages
-
-from routemaster import version
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
