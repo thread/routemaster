cd5fa2f8afdb4dc947f067cd2047bbbdd38d32ba
Dan Palmer <dan@danpalmer.me>
2018-04-23 21:17:03 +0100

    Set up prometheus metrics

--- plugins/routemaster-prometheus/routemaster_prometheus/__init__.py
+++ plugins/routemaster-prometheus/routemaster_prometheus/__init__.py
@@ -17,6 +17,24 @@ exceptions = Counter(
     ('type',),
 )
 
+cron_jobs_processed = Counter(
+    'cron_jobs_processed',
+    "Cron jobs processed",
+    ('fn_name', 'state_machine', 'state'),
+)
+
+webhooks_triggered = Counter(
+    'webhooks_triggered',
+    "Webhooks triggered",
+    ('state_machine', 'state'),
+)
+
+feed_requests = Counter(
+    'feed_requests',
+    "Feed requests",
+    ('feed_url', 'state_machine', 'state'),
+)
+
 
 class PrometheusLogger(BaseLogger):
     """Instruments Routemaster with Prometheus."""
@@ -34,21 +52,29 @@ class PrometheusLogger(BaseLogger):
         """Send cron exceptions to Prometheus."""
         with exceptions.labels(type='cron').count_exceptions():
             yield
+            cron_jobs_processed.labels(
+                fn_name=fn_name,
+                state_machine=state_machine.name,
+                state=state.name,
+            ).inc()
 
     @contextlib.contextmanager
     def process_webhook(self, state_machine, state):
         """Send webhook request exceptions to Prometheus."""
         with exceptions.labels(type='webhook').count_exceptions():
             yield
+            webhooks_triggered.labels(
+                state_machine=state_machine.name,
+                state=state.name,
+            ).inc()
 
     @contextlib.contextmanager
     def process_feed(self, state_machine, state, feed_url):
         """Send feed request exceptions to Prometheus."""
         with exceptions.labels(type='feed').count_exceptions():
             yield
-
-    @contextlib.contextmanager
-    def process_request(self):
-        """Send API request exceptions to Prometheus."""
-        with exceptions.labels(type='request').count_exceptions():
-            yield
+            feed_requests.labels(
+                feed_url=feed_url,
+                state_machine=state_machine.name,
+                state=state.name,
+            ).inc()
