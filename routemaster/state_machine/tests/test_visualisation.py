13cb98f976986939ef424e2d182c6d40d1cbc376
Peter Law <PeterJCLaw@gmail.com>
2018-05-02 16:21:15 +0100

    Make this test pass

--- routemaster/state_machine/tests/test_visualisation.py
+++ routemaster/state_machine/tests/test_visualisation.py
@@ -1,5 +1,5 @@
 from routemaster.state_machine import convert_to_network
 
 
-def test_convert_to_network(app_config):
-    convert_to_network(app_config.config.state_machines['test_machine'])
+def test_convert_to_network(app):
+    convert_to_network(app.config.state_machines['test_machine'])
