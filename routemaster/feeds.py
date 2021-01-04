c4ff5ea827a930cdcfc74f38263c425fe51bed8c
Peter Law <PeterJCLaw@gmail.com>
2021-01-04 18:53:46 +0000

    Update the version of mypy we're using
    The only fix needed is to change our Feed dataclass so that it
    doesn't abuse the InitVar mechanism. Instead we declare 'data'
    as an instance variable explicitly as part of __post_init__,
    which mypy does understand and correctly allows.

--- routemaster/feeds.py
+++ routemaster/feeds.py
@@ -3,7 +3,7 @@ import threading
 from typing import Any, Dict, Callable, Optional
 
 import requests
-from dataclasses import InitVar, dataclass
+from dataclasses import dataclass
 
 from routemaster.utils import get_path, template_url
 
@@ -37,7 +37,10 @@ class Feed:
     """A feed fetcher, able to retreive a feed and read keys out of it."""
     url: str
     state_machine: str
-    data: InitVar[Optional[Dict[str, Any]]] = None
+
+    def __post_init__(self) -> None:
+        """Record that we have some data too."""
+        self.data: Optional[Dict[str, Any]] = None
 
     def prefetch(
         self,
