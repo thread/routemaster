19d86f91372c32fc6860d254d1f171e10381d002
Peter Law <PeterJCLaw@gmail.com>
2021-01-08 17:09:11 +0000

    Ensure the proper readme files are picked up
    Assuming the working directory is correct doesn't always work.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 """Package setup."""
 
+from pathlib import Path
+
 import version
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+long_description = (Path(__file__).parent / 'README.md').read_text()
 
 setup(
     name='routemaster',
