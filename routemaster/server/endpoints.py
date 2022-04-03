23689dcfb07f95f3e3aa2529344427d0df6c9bfe
Peter Law <PeterJCLaw@gmail.com>
2022-04-03 12:16:43 +0100

    Fix lints

--- routemaster/server/endpoints.py
+++ routemaster/server/endpoints.py
@@ -81,7 +81,7 @@ def view_state_machine(state_machine_name):
 
     try:
         state_machine = app.config.state_machines[state_machine_name]
-    except KeyError as k:
+    except KeyError:
         msg = f"State machine '{state_machine_name}' does not exist"
         abort(404, msg)
 
